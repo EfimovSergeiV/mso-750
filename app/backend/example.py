33141011256aff2a8c8cd53d5c3e77239188afd1
anon <sys@tehnosvar.ru>
2022-04-29 16:44:21 +0300

    add: multi-thread example

--- app/backend/example.py
+++ app/backend/example.py
@@ -6,6 +6,7 @@ import datetime, random
 import serial
 from backend.decorators import benchmark
 
+
 class Example(QObject):
     """ Класс демонстрации """
 
@@ -15,21 +16,22 @@ class Example(QObject):
         self.timer.timeout.connect(lambda: self.SensorOne())
         self.timer.start(600)
 
+
     # Signals
     sensorOne = Signal(str)
 
 
     def SensorOne(self):
         """ Пример сигнала """
-        port = serial.Serial('/dev/ttyUSB0', baudrate=38400)
-        data = port.read(13).hex()
-        bytes = [data[i:i+2] for i in range(0, len(data), 2)]
-        print(f'data: {bytes[0]} {bytes[1]} {bytes[2]} {bytes[3]} {bytes[4]} {bytes[5]} {bytes[6]} {bytes[7]} {bytes[8]} {bytes[9]} {bytes[10]} {bytes[11]} {bytes[12]}')
+        # port = serial.Serial('/dev/ttyUSB0', baudrate=38400)
+        # data = port.read(13).hex()
+        # bytes = [data[i:i+2] for i in range(0, len(data), 2)]
+        # print(f'data: {bytes[0]} {bytes[1]} {bytes[2]} {bytes[3]} {bytes[4]} {bytes[5]} {bytes[6]} {bytes[7]} {bytes[8]} {bytes[9]} {bytes[10]} {bytes[11]} {bytes[12]}')
         
-        if bytes[1] == '0b' and bytes[2] == '77':
-            print(f'\nПолучен пакет данных от модуля 0B77\n')
+        # if bytes[1] == '0b' and bytes[2] == '77':
+        #     print(f'\nПолучен пакет данных от модуля 0B77\n')
 
-            value_list = range(300, 500)
-            a = random.randint(0, len(value_list)-1)
-            formatDate = str(value_list[a])
-            self.sensorOne.emit(formatDate)
+        value_list = range(300, 500)
+        a = random.randint(0, len(value_list)-1)
+        formatDate = str(value_list[a])
+        self.sensorOne.emit(formatDate)
--- app/main.py
+++ app/main.py
@@ -10,16 +10,24 @@ from backend.core import parse_data
 from backend.example import Example
 os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
 
-
+import serial
 
 if __name__ == "__main__":
     app = QApplication([])
+
+
+    # port = serial.Serial('/dev/ttyUSB0', baudrate=38400)
+    # data = port.read(13).hex()
+    # bytes = [data[i:i+2] for i in range(0, len(data), 2)]
+    # print(f'data: {bytes[0]} {bytes[1]} {bytes[2]} {bytes[3]} {bytes[4]} {bytes[5]} {bytes[6]} {bytes[7]} {bytes[8]} {bytes[9]} {bytes[10]} {bytes[11]} {bytes[12]}')
+    
+    # if bytes[1] == '0b' and bytes[2] == '77':
+    #     print(f'\nПолучен пакет данных от модуля 0B77\n')
+
     engine = QQmlApplicationEngine()
     engine.load(os.fspath(Path(__file__).resolve().parent / "src/main.qml"))
 
     example = Example()
     engine.rootContext().setContextProperty("example", example)
-
-    # parse_data()
 
     sys.exit(app.exec_())
